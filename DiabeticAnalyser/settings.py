15d3c240a3b7229bc533ef270da990b81d68b143
Zyph3r <72371058+z3yph3r@users.noreply.github.com>
2023-08-13 19:10:46 +0530

    Update settings.py

--- DiabeticAnalyser/settings.py
+++ DiabeticAnalyser/settings.py
@@ -118,9 +118,6 @@ USE_TZ = True
 import os
 STATIC_URL = '/static/'
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
